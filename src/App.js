07d999e607f4c826533f57296f394f05fed414fb
Merisabk <bishwakarma.merisa@gmail.com>
2021-08-10 08:37:24 +0100

    added bootsstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
-import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
-      <Weather />
-      <footer>
-        This project was coded by Merisa and is{" "}
-        <a href="https://github.com/Merisabk/react-weather-app" target="_blank">
-          open sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello World!!</h1>
+
+        <footer>
+          This project was coded by Merisa and is{" "}
+          <a
+            href="https://github.com/Merisabk/react-weather-app"
+            target="_blank rel= noreferrer"
+          >
+            open sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
