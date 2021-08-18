83e5895a7510a3191fcbc58b0e0fb7cf4e7d2330
Merisabk <bishwakarma.merisa@gmail.com>
2021-08-18 11:43:39 +0100

    added the html and css

--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,47 @@
-import axios from "axios";
 import React from "react";
 import "./Weather.css";
 
-export default function Weather(props) {
+export default function Weather() {
   return (
     <div className="Weather">
-      <input type="search" placeholder="Enter a city..." />
-      <input type="Submit" value="Search" />
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="Search"
+              placeholder="Enter a city..."
+              autoFocus="on"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="Submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
       <h1>Porto</h1>
-      <h2>Tuesday 10:00</h2>
       <ul>
-        <li>Clear sky</li>
-        <li>22ºC</li>
-        <li>Humidity: 70%</li>
-        <li>Wind: 3 km/h</li>
+        <li>Wednesday 07:00</li>
+        <li>Sunny</li>
       </ul>
-
       <div className="row">
         <div className="col-6">
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="cloudy" />
-          6ºC
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
+              className="float-left"
+            />{" "}
+            <span className="temperature">16</span>
+            <span className="unit">ºC</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
             <li>Wind: 13 km/h</li>
           </ul>
