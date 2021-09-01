ba1ba897b30dfc6f5bc130ec8ef35c2891537ee2
Merisabk <bishwakarma.merisa@gmail.com>
2021-09-01 09:53:46 +0100

    fixed weather icon codes

--- src/Weathericon.js
+++ src/Weathericon.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function weatherIcon(props) {
-  console.log(props.code);
+export default function weathericon(props) {
   const codeMapping = {
     "01d": "CLEAR_DAY",
     "01n": "CLEAR_NIGHT",
@@ -14,12 +13,12 @@ export default function weatherIcon(props) {
     "04n": "CLOUDY",
     "09d": "RAIN",
     "09n": "RAIN",
-    "010d": "RAIN",
-    "010n": "RAIN",
-    "011d": "RAIN",
-    "011n": "RAIN",
-    "013d": "SNOW",
-    "013n": "SNOW",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
     "50d": "FOG",
     "50n": "FOG",
   };
